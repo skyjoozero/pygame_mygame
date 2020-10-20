21052728ed73bcca3037cb8e6b7104fd05b5a131
skyjoozero <skyjoozero@naver.com>
2020-10-20 23:45:13 +0900

    20201020

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+################################생성된 객체가 처음것을 따라가게 만들기###################################
 pygame.init()
 
 
@@ -18,10 +18,16 @@ red_size = red.get_rect().size
 red_width = red_size[0]
 red_height = red_size[1]
 
-player = []
+green = pygame.image.load("images\\green.png")
+green_size = green.get_rect().size
+green_width = green_size[0]
+green_height = green_size[1]
+
+players = []
+score = 1
 player_x_pos = random.randint(1, 30)
 player_y_pos = random.randint(1, 20)
-player.append([player_x_pos, player_y_pos])
+players.append([player_x_pos, player_y_pos])
 
 to_x = 0
 to_y = 0
@@ -32,10 +38,11 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("game")
 clock = pygame.time.Clock()
 
-
+enemy_on = True
 
 while running:
     dt = clock.tick(20)
+    temp = score
     for i in range(1,31):
         for j in range(1,21):
             screen.blit(white, ((i - 1) * white_width, (j - 1) * white_width))
@@ -60,25 +67,72 @@ while running:
 
 
 
+    ############랜덤등장###############################
+    if enemy_on:
+        rand_x = random.randint(0,29)
+        rand_y = random.randint(0,19)
+        
+        enemy_on = False
     
+    
+    
+    ############캐릭터 좌표설정####################
 
-    screen.blit(red, ((player_x_pos - 1) * white_width, (player_y_pos - 1) * white_width))
+    
+    
+    # players = [[p[0][0] + to_x, p[0][1] + to_y] for p in players]
+
+    players[0][0] += to_x
+    players[0][1] += to_y
+
+    for i in range(0, score):
+        player_x = int(players[i][0])
+        player_y = int(players[i][1])
+        screen.blit(red, ((player_x - 1) * white_width, (player_y - 1) * white_width))
+        if players[i][0] < 0:
+            players[i][0] = 30
+        if players[i][0] > 30:
+            players[i][0] = 0
+        if players[i][1] < 0:
+            players[i][1] = 20
+        if players[i][1] > 20:
+            players[i][1] = 0
+    
+    screen.blit(green, (rand_x * green_width, rand_y * green_height))
 
 
-    # player_x_pos += to_x
-    # player_y_pos += to_y
-    player = [[p[0] + to_x, p[1] + to_y] for p in player]
-    print(player)
+    # players_x_pos += to_x
+    # players_y_pos += to_y
+    # if score > temp:
+    #     players.append([[players_x_pos], [players_y_pos]])
+    # print(len(players))
+    # print(players)
 
 
-    if player_x_pos < 0:
-        player_x_pos = 30
-    if player_x_pos > 30:
-        player_x_pos = 0
-    if player_y_pos < 0:
-        player_y_pos = 20
-    if player_y_pos > 20:
-        player_y_pos = 0
+    ################점수추가 충돌계산 충동설정 enemy_on설정 score += 1######################
+
+    red_rect = red.get_rect()
+    red_rect.left = (players[0][0] - 1) * red_width
+    red_rect.top = (players[0][1] - 1) * red_height
+
+    green_rect = green.get_rect()
+    green_rect.left = rand_x * green_width
+    green_rect.top = rand_y * green_height
+    
+    if red_rect.colliderect(green_rect):
+        score += 1
+        enemy_on = True
+        if to_x > 0 and to_y == 0:
+            players.append([players[int(len(players)) - 1][0] - 1, players[int(len(players)) - 1][1]])
+        if to_x < 0 and to_y == 0:
+            players.append([players[int(len(players)) - 1][0] + 1, players[int(len(players)) - 1][1]])
+        if to_x == 0 and to_y > 0:
+            players.append([players[int(len(players)) - 1][0], players[int(len(players)) - 1][1] + 1])
+        if to_x == 0 and to_y < 0:
+            players.append([players[int(len(players)) - 1][0], players[int(len(players)) - 1][1] - 1])
+    
+    
+    print(players)
 
 
     pygame.display.update()
